96761da714e7d0e7e2b8e4c72bd2592d26d7f9fa
zekunjin <zekun.jin@foxmail.com>
2023-08-18 11:03:52 +0800

    chore: update swagger interface

---
+++ packages/swagger/types.d.ts
@@ -0,0 +1,37 @@
+type DataType = 'integer' | 'int32'
+type RequestMethod = 'post' | 'get' | 'delete' | 'put' | 'update'
+type RequestConsume = 'application/json' | 'application/' | 'delete' | 'put' | 'update'
+type RequestIn = 'body' | 'query' | 'params'
+
+interface Swagger {
+  basePath: string
+  host: string
+  swagger: string
+  definitions: SwaggerDefinations
+  paths: Record<string, Partial<Record<RequestMethod, SwaggerRequest>>>
+}
+
+interface SwaggerDefinations {
+  ApiResponse: SwaggerApiResponseDefination
+}
+
+interface SwaggerApiResponseDefination {
+  type: 'object'
+  properties: Record<string, { type: DataType, format?: DataType }>
+}
+
+interface SwaggerRequest {
+  consumes: RequestConsume[]
+  description: string
+  operationId: string
+  parameters: SwaggerRequestParameter[]
+  produces: RequestConsume[]
+  summary: string
+  tags: string[]
+}
+
+interface SwaggerRequestParameter {
+  description: string
+  in: RequestIn
+  required: boolean
+}
